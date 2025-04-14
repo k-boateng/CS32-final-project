1df421be72170d949937c5ae5adaa876032dcac2
k-boateng <kwameboateng.kb@proton.me>
2025-04-14 05:20:22 +0000

    Added the other folders that will be needed

--- config.py
+++ config.py
@@ -1,14 +1,24 @@
+import os
 from pathlib import Path
 
+#Organization
 
 #If new user, this folder will not exist.
+base_directory = Path.home() / "messageapp"
 
 def is_new_user():
-    
-    folder_path = Path.home() / "messageapp" #Checks if this folder exists
-    
-    if not folder_path.exists(): #Create a new folder
-        folder_path.mkdir(parents=True)
 
+     #Checks if this folder exists
+    if not base_directory.exists(): #Create a new folder
+        base_directory.mkdir(parents=True)
+
+        new_folders = ["Messages", "Friends", "Config"]
+
+        for folder in new_folders:
+            folder_path = base_directory / folder  # Append the folder name to the base directory
+            folder_path.mkdir(parents=True, exist_ok=True)
     return None
+
+def starting_page():
+    contents = os.listdir(base_directory/"Friends")
 
